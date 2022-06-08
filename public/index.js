f5ba820bf5abfd6e1db5bea6fe06af07c41d0183
ImBrunoHG <38635661+ImBrunoHG@users.noreply.github.com>
2022-06-08 17:07:08 -0300

    Projeto Listaço

---
+++ public/index.js
@@ -0,0 +1,20 @@
+function drag(e){
+    e.dataTransfer.setData("text", e.target.id);
+}
+function drop(e){
+    e.preventDefault();
+    var data = e.dataTransfer.getData("text");
+    e.target.appendChild(document.getElementById(data))
+}
+function allowDrop(e){
+    e.preventDefault()
+}
+function newList(){
+    const listbox = document.getElementById('listbox')
+    const final = document.getElementById('final')
+    var list = document.createElement('div')
+    list.className = "container centralized list"
+    list.setAttribute('ondragover', "allowDrop(event)")
+    list.setAttribute('ondrop', "drop(event)")
+    listbox.insertBefore(list, final)
+}
